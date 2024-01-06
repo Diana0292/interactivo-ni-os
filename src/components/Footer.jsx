34789aa1748c3fcf6eeeed149f7992d22abd230f
Diana Martinez <seb.paisa1992@gmail.com>
2024-01-06 21:07:37 +0100

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,21 @@
-import "./components.css";
+import {Link } from "react-router-dom";
+import "./Footer.css";
 
 export const Footer = () =>{
     return (
-        <footer>
-            footer
+        <footer className="footer">
+            <h3 className="momentos">momentos del dia</h3>
+            <ul className="footer-lista">
+                <li>
+                    <Link>inicio</Link>
+                </li>
+                <li>
+                    <Link>Mi Lista</Link>
+                </li>
+                <li>
+                    <Link>Perfil</Link>
+                </li>
+            </ul>
         </footer>
     )
 }
